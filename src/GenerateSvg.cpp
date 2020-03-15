1cf70799f588cc07dc850125f79348cc6423b1ee
Nathan Marshall <NathanMarshall@cmail.carleton.ca>
2020-03-14 21:34:20 -0400

    Diagonals are connected in the SVG with simple polygons

--- src/GenerateSvg.cpp
+++ src/GenerateSvg.cpp
@@ -31,16 +31,37 @@ int generateSvg(const cv::Mat& src, const PixelGraph* graph, const char* outPath
             sstr << "    <rect x = \"" << c << "\" y = \"" << r << "\" width = \"1\" height = \"1\" fill = \""
                 << colorStr << "\" stroke = \"" << colorStr << "\" stroke-width = \"" << strokeWidth <<"\"/>"
                 << std::endl;
+        }
+    }
 
-            /*if (graph->graph->at(r)->at(c)->topLeft) {
+    for (int r = 0; r < src.rows; r++) {
+        for (int c = 0; c < src.cols; c++) {
+            cv::Vec3b bgr = src.at<cv::Vec3b>(r, c);
+
+            std::string colorStr = (std::stringstream() << "rgb(" << +bgr[2] << ", " << +bgr[1] << ", "
+                << +bgr[0] << ")").str();
+
+            // top left diagonals
+            if (graph->graph->at(r)->at(c)->topLeft) {
                 sstr << "    <polygon points = \""
-                    << (c) << "," << (r + 1) << " "
-                    << (c - 1) << "," << (r) << " "
-                    << (c) << "," << (r - 1) << " "
-                    << (c + 1) << "," << (r) << "\" fill = \""
+                    << (c) << "," << (r + 0.5f) << " "
+                    << (c - 0.5f) << "," << (r) << " "
+                    << (c) << "," << (r - 0.5f) << " "
+                    << (c + 0.5f) << "," << (r) << "\" fill = \""
                     << colorStr << "\" stroke = \"" << colorStr << "\" stroke-width = \"0\"/>"
                     << std::endl;
-            }*/
+            }
+
+            // top right diagonals
+            if (graph->graph->at(r)->at(c)->topRight) {
+                sstr << "    <polygon points = \""
+                    << (c + 0.5f) << "," << (r) << " "
+                    << (c + 1) << "," << (r - 0.5f) << " "
+                    << (c + 1.5f) << "," << (r) << " "
+                    << (c + 1) << "," << (r + 0.5f) << "\" fill = \""
+                    << colorStr << "\" stroke = \"" << colorStr << "\" stroke-width = \"0\"/>"
+                    << std::endl;
+            }
         }
     }
 
@@ -57,48 +78,50 @@ int generateSvg(const cv::Mat& src, const PixelGraph* graph, const char* outPath
     if (DEBUG_SHOW_GRAPH) {
         for (int r = 0; r < src.rows; r++) {
             for (int c = 0; c < src.cols; c++) {
+                const float graphStrokeWidth = 0.05f;
+
                 if (graph->graph->at(r)->at(c)->top) {
                     sstr << "    <line x1 = \"" << (c + 0.5f) << "\" y1 = \"" << (r + 0.5f)
                         << "\" x2 = \"" << (c + 0.5f) << "\" y2 = \"" << (r - 0.5f)
-                        << "\" stroke = \"red\" stroke-width = \"0.2\"/>" << std::endl;
+                        << "\" stroke = \"red\" stroke-width = \"" << graphStrokeWidth << "\"/>" << std::endl;
                 }
                 if (graph->graph->at(r)->at(c)->bottom) {
                     sstr << "    <line x1 = \"" << (c + 0.5f) << "\" y1 = \"" << (r + 0.5f)
                         << "\" x2 = \"" << (c + 0.5f) << "\" y2 = \"" << (r + 1.5f)
-                        << "\" stroke = \"green\" stroke-width = \"0.2\"/>" << std::endl;
+                        << "\" stroke = \"green\" stroke-width = \"" << graphStrokeWidth << "\"/>" << std::endl;
                 }
 
                 if (graph->graph->at(r)->at(c)->left) {
                     sstr << "    <line x1 = \"" << (c + 0.5f) << "\" y1 = \"" << (r + 0.5f)
                         << "\" x2 = \"" << (c - 0.5f) << "\" y2 = \"" << (r + 0.5f)
-                        << "\" stroke = \"orange\" stroke-width = \"0.2\"/>" << std::endl;
+                        << "\" stroke = \"orange\" stroke-width = \"" << graphStrokeWidth << "\"/>" << std::endl;
                 }
                 if (graph->graph->at(r)->at(c)->right) {
                     sstr << "    <line x1 = \"" << (c + 0.5f) << "\" y1 = \"" << (r + 0.5f)
                         << "\" x2 = \"" << (c + 1.5f) << "\" y2 = \"" << (r + 0.5f)
-                        << "\" stroke = \"purple\" stroke-width = \"0.2\"/>" << std::endl;
+                        << "\" stroke = \"purple\" stroke-width = \"" << graphStrokeWidth << "\"/>" << std::endl;
                 }
 
                 if (graph->graph->at(r)->at(c)->topLeft) {
                     sstr << "    <line x1 = \"" << (c + 0.5f) << "\" y1 = \"" << (r + 0.5f)
                         << "\" x2 = \"" << (c - 0.5f) << "\" y2 = \"" << (r - 0.5f)
-                        << "\" stroke = \"blue\" stroke-width = \"0.2\"/>" << std::endl;
+                        << "\" stroke = \"blue\" stroke-width = \"" << graphStrokeWidth << "\"/>" << std::endl;
                 }
                 if (graph->graph->at(r)->at(c)->bottomRight) {
                     sstr << "    <line x1 = \"" << (c + 0.5f) << "\" y1 = \"" << (r + 0.5f)
                         << "\" x2 = \"" << (c + 1.5f) << "\" y2 = \"" << (r + 1.5f)
-                        << "\" stroke = \"cyan\" stroke-width = \"0.2\"/>" << std::endl;
+                        << "\" stroke = \"cyan\" stroke-width = \"" << graphStrokeWidth << "\"/>" << std::endl;
                 }
 
                 if (graph->graph->at(r)->at(c)->topRight) {
                     sstr << "    <line x1 = \"" << (c + 0.5f) << "\" y1 = \"" << (r + 0.5f)
                         << "\" x2 = \"" << (c + 1.5f) << "\" y2 = \"" << (r - 0.5f)
-                        << "\" stroke = \"yellow\" stroke-width = \"0.2\"/>" << std::endl;
+                        << "\" stroke = \"yellow\" stroke-width = \"" << graphStrokeWidth << "\"/>" << std::endl;
                 }
                 if (graph->graph->at(r)->at(c)->bottomLeft) {
                     sstr << "    <line x1 = \"" << (c + 0.5f) << "\" y1 = \"" << (r + 0.5f)
                         << "\" x2 = \"" << (c - 0.5f) << "\" y2 = \"" << (r + 1.5f)
-                        << "\" stroke = \"magenta\" stroke-width = \"0.2\"/>" << std::endl;
+                        << "\" stroke = \"magenta\" stroke-width = \"" << graphStrokeWidth << "\"/>" << std::endl;
                 }
             }
         }
