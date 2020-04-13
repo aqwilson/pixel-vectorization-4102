f1158a83c586a908f6fe764e5ef0b25939904dd2
Mitchell Blanchard <mitchellblanchard3@gmail.com>
2020-04-13 19:23:23 -0400

    Splines!

--- include/Polygon.h
+++ include/Polygon.h
@@ -9,6 +9,13 @@ using Contour = std::vector<cv::Point2f>;
 class Polygon {
 
 public:
+    class ControlAnchor {
+        public:
+            cv::Point2f anchorPoint;
+            cv::Point2f previousControlPoint;
+            cv::Point2f nextControlPoint;
+    };
+
     Contour contour;
     cv::Vec3b color;
     int depth;
@@ -16,6 +23,22 @@ public:
     Polygon();
     ~Polygon();
 
+    template <class T>
+    static T GetPoint(int index, std::vector<T> anchors){
+        if (index > (int)anchors.size() - 1) {
+            index = index - anchors.size();
+        }
+        else if (index < 0) {
+            index = index + anchors.size();
+        }
+
+        return anchors[index];
+    }
+
+    static std::vector<Polygon::ControlAnchor> Polygon::GenerateControlAnchors(Contour anchors);
+
 private:
+    static ControlAnchor Polygon::CreateControlAnchor(cv::Point2f currentPoint, cv::Point2f previousPoint, cv::Point2f nextPoint);
+
 
 };
